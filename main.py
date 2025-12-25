15f37f58f5e4ed131b6397b2c96403460720d7fd
Arnav <arnavbhatnagar510@gmail.com>
2025-12-25 18:22:42 +0530

    Fixed critical logical errors

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ def c(p):
         l.append(f"{i:0{p}b}")
     x = l.copy()
     for j in l:
+        if j.count("1") <= 1:
+            x.remove(j)
+            continue
+        pre = j.find("1")-1
         for idx, k in enumerate(j):
-            if idx == 0 or idx == len(j) - 1:
-                continue
-            if k == "1" and j[idx-1] != "1" and j[idx+1] != "1":
-                x.remove(j)
-                break
+            if k == "1":
+                if idx != pre + 1:
+                    x.remove(j)
+                    break
+                pre += 1
     x.sort(key = lambda x: x.count("0"))
     return x
 
@@ -25,17 +29,14 @@ def longest_subarray_k_distinct(n, k):
                 g.append(n[idx])
         count = 0
         z = []
-        for idx, x in enumerate(g):
-            if x in z:
-                continue
-            for y in g[:idx] + g[idx+1:]:
-                if x == y:
-                    z.append(x)
-                    count += 1
+        for x in g:
+            if x not in z and g.count(x) > 1:
+                count += g.count(x)-1
+                z.append(x)
         count = len(g) - count
         if count == k:
             return len(g)
     return 0
 
 if __name__ == "__main__":
-    print(longest_subarray_k_distinct([1,2,3,4,3,6], 3))
+    print(longest_subarray_k_distinct([1,2,4,5,3,4,5,6,4,9,7,5], 4))
