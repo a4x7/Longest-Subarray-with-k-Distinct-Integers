db86060591ced0df7c199ffddd383f1b5a96919f
Arnav <arnavbhatnagar510@gmail.com>
2025-12-25 19:19:44 +0530

    Fixed condition for k = 1, introduced user chosen length of the input list and the value of k, creation of a random list of user-defined length, added constraint of length <= 20, gave description to constraint errors, fixed README and mentioned the right required inputs, parameters, complexity, outputs and the objective

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-<h1><i>Longest Subarray with k Distinct Integers</i></h1>
-Input: List <br>
-Parameters: k <= Length of List <br>
+<h1><i>Length of Longest Contiguous Subarray with k Distinct Integers</i></h1>
+Input: Length of List, k <br>
+Parameters: k <= Length of List <= 20 <br>
 Complexity: O(2^n*n) <br>
-Output: Longest List with k Distinct Numbers
+Output: The Input List, The Longest Contiguous List with k Distinct Numbers and it's Length
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+import random
+
 def c(p):
     l = []
     for i in range(2**p):
         l.append(f"{i:0{p}b}")
     x = l.copy()
     for j in l:
-        if j.count("1") <= 1:
-            x.remove(j)
-            continue
         pre = j.find("1")-1
         for idx, k in enumerate(j):
             if k == "1":
@@ -19,9 +18,7 @@ def c(p):
 
 def longest_subarray_k_distinct(n, k):
     if k > len(n):
-        raise ValueError
-    if k == 1:
-        return 1
+        raise ValueError("k cannot have a value which is greater than the length of the input list!")
     for i in c(len(n)):
         g = []
         for idx, j in enumerate(i):
@@ -35,8 +32,18 @@ def longest_subarray_k_distinct(n, k):
                 z.append(x)
         count = len(g) - count
         if count == k:
-            return len(g)
-    return 0
+            return n, g, len(g)
+    return n, [], 0
+
+def main():
+    l = []
+    length = int(input())
+    if length > 20:
+        raise ValueError("Length of input list should not exceed 20!")
+    for i in range(length):
+        l.append(random.randint(0,9))
+    k = int(input())
+    print(longest_subarray_k_distinct(l, k))
 
 if __name__ == "__main__":
-    print(longest_subarray_k_distinct([1,2,4,5,3,4,5,6,4,9,7,5], 4))
+    main()
